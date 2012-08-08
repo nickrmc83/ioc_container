899cf48f7b852023e6256e656bd3150dbfbdb8b0
Nicholas Smith <nickrmc83@gmail.com>
2012-08-08 07:01:09 +0100

    Fixed std::tuple unwrapping. Howver, it could do with some TLC as currently you have to declare the callable_type, return type and arg types. It would be nice if we did not have to specify the callable type as it should be able to be idetified from the return type and arg types.

--- tuple_helper.h
+++ tuple_helper.h
@@ -1,13 +1,30 @@
 /// Helper functions for dealing with tuples.
 /// Copyright Nicholas Smith 2012 All right reserved
 
-template <>
+#include <tuple>
+#include <stdint.h>
+#include <exception>
+
+template<std::size_t index>
+struct tuple_unwrap_impl;
+
+template<>
 struct tuple_unwrap_impl<0>
 {
-    template<typename callable_type, typename return_type, typename tupe_type, typename ...arg_types>
+    template<typename callable_type, typename return_type, typename tuple_type, typename ...arg_types>
         static return_type call( callable_type callable, tuple_type tuple, arg_types ...args )
         {
-            return callable( std::get<0>( tuple ), args... );
+            static_assert( true, "Can't unwrap an empty tuple" );
+        }
+};
+
+template<>
+struct tuple_unwrap_impl<1>
+{
+    template<typename callable_type, typename return_type, typename tuple_type, typename ...arg_types>
+        static return_type call( callable_type callable, tuple_type tuple, arg_types ...args )
+        {
+            return callable( args..., std::get<sizeof...(arg_types)>(tuple) );
         }
 };
 
@@ -15,19 +32,21 @@ template<size_t index>
 struct tuple_unwrap_impl
 {
     template<typename callable_type, typename return_type, typename tuple_type, typename ...arg_types>
-        static return_type call( callable_type callable, tuple_type tuple, arg_types ...args )
+        static return_type call( callable_type callable, const tuple_type &tuple, arg_types ...args )
         {
-            return template_unwrap_impl<index -1>
-                ::call( callable, tuple, std::get<index>(tuple).value, args... ); 
+            typedef typename std::tuple_element<sizeof...(arg_types), tuple_type>::type this_type;
+            return tuple_unwrap_impl<index - 1>
+                ::template call<callable_type, return_type, tuple_type, arg_types..., this_type>
+                ( callable, tuple, args..., std::get<sizeof...(arg_types)>(tuple) ); 
         }
 };
 
 struct tuple_unwrap
 {
-    template<typename callable_type, typename return_type, typename tuple_type>
-        static return_type call( callable_type callable, tuple_type tuple )
+    template<typename callable_type, typename return_type, typename ...arg_types>
+        static return_type call( callable_type callable, const std::tuple<arg_types...> &tuple )
         {
-            return tupe_unwrap_impl<tuple_type::size - 1>::
-                call<callable_type, return_type, tuple_type>( callable, tuple );
+            return tuple_unwrap_impl<sizeof...(arg_types)>::
+                template call<callable_type, return_type, std::tuple<arg_types...>>( callable, tuple );
         }
 };
